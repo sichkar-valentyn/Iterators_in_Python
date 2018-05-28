089ddf10ce48be55070b1bc45a34ba9e7052359b
Valentyn N Sichkar <34546286+sichkar-valentyn@users.noreply.github.com>
2018-05-28 18:29:33 +0300

    Update iterators.py

--- iterators.py
+++ iterators.py
@@ -1,3 +1,15 @@
+# File: Iterators_in_Python.py
+# Description: Examples on how to use iterators in Python
+# Environment: PyCharm and Anaconda environment
+#
+# MIT License
+# Copyright (c) 2018 Valentyn N Sichkar
+# github.com/sichkar-valentyn
+#
+# Reference to:
+# [1] Valentyn N Sichkar. Examples on how to use iterators in Python // GitHub platform [Electronic resource]. URL: https://github.com/sichhttps://github.com/sichkar-valentyn/Iterators_in_Python (date of access: XX.XX.XXXX)
+
+
 # Showing the standard way how to iterate
 # And explaining how it is done in Python
 
