ca93dcfb2bf08c409ada41c0f702413475a63f14
Valentyn N Sichkar <34546286+sichkar-valentyn@users.noreply.github.com>
2018-05-28 18:30:29 +0300

    Update iterators_2.py

--- iterators_2.py
+++ iterators_2.py
@@ -1,3 +1,15 @@
+# File: iterators_2.py
+# Description: Examples on how to use iterators in Python
+# Environment: PyCharm and Anaconda environment
+#
+# MIT License
+# Copyright (c) 2018 Valentyn N Sichkar
+# github.com/sichkar-valentyn
+#
+# Reference to:
+# [1] Valentyn N Sichkar. Examples on how to use iterators in Python // GitHub platform [Electronic resource]. URL: https://github.com/sichhttps://github.com/sichkar-valentyn/Iterators_in_Python (date of access: XX.XX.XXXX)
+
+
 from random import random
 
 
@@ -31,53 +43,3 @@ print(next(x))
 # Iterating the class
 for x in RandomIterator(3):
     print(x)
-
-
-# Implementing the task about multi-filter
-def m2(x):
-    return x % 2 == 0
-
-
-def m3(x):
-    return x % 3 == 0
-
-
-def m5(x):
-    return x % 5 == 0
-
-
-a = [i for i in range(31)]
-
-
-class multifilter:
-    def judge_half(self, pos, neg):
-        return pos >= neg
-
-    def judge_any(self, pos, neg):
-        return pos >= 1
-
-    def judge_all(self, pos, neg):
-        return neg == 0
-
-    def __init__(self, iterable, *funcs, judge=judge_any):
-        self.iterable = iterable
-        self.funcs = funcs
-        self.judge = judge
-
-    def __iter__(self):
-        for e in self.iterable:
-            pos = 0
-            neg = 0
-            for f in self.funcs:
-                if f(e):
-                    pos += 1
-                else:
-                    neg += 1
-            if self.judge(pos, neg):
-                yield e
-
-
-print(list(multifilter(a, m2, m3, m5)))
-
-
-
